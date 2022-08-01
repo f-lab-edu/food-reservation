eeb77279c9fdff6bf41677a429fed9e17107e22a
Minseong Kim <42605239+SOULIKK@users.noreply.github.com>
2022-08-01 10:26:54 +0900

    [Feat] 매뉴(Item) 등록, 삭제 기능 구현 (#20)
    * Feat: 매뉴(Item) 등록, 삭제 기능 구현
    
    * TEST: 매뉴(Item) 등록, 삭제 유닛 테스트
    
    * Refactor: 충돌 해결

--- app/src/main/java/com/foodlab/foodReservation/item/controller/ItemController.java
+++ app/src/main/java/com/foodlab/foodReservation/item/controller/ItemController.java
@@ -1,12 +1,19 @@
 package com.foodlab.foodReservation.item.controller;
 
+import com.foodlab.foodReservation.item.dto.request.CreateItemRequest;
 import com.foodlab.foodReservation.item.dto.request.UpdateItemRequest;
+import com.foodlab.foodReservation.item.dto.response.CreateItemResponse;
+import com.foodlab.foodReservation.item.dto.response.DeleteItemResponse;
 import com.foodlab.foodReservation.item.dto.response.UpdateItemResponse;
 import com.foodlab.foodReservation.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -20,5 +27,18 @@ public class ItemController {
     @PutMapping("/items/{itemId}")
     public UpdateItemResponse updateItem(@PathVariable("itemId") Long itemId, @RequestBody @Valid UpdateItemRequest updateItemRequest) {
         return itemService.updateItem(itemId, updateItemRequest);
+    }
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/items")
+    public CreateItemResponse createItem(@RequestBody @Valid CreateItemRequest createItemRequest) {
+        return itemService.createItem(createItemRequest);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/items/{itemId}")
+    public DeleteItemResponse deleteItem(@PathVariable Long itemId) {
+        return itemService.deleteItem(itemId);
     }
 }
--- app/src/main/java/com/foodlab/foodReservation/item/service/ItemService.java
+++ app/src/main/java/com/foodlab/foodReservation/item/service/ItemService.java
@@ -1,18 +1,25 @@
 package com.foodlab.foodReservation.item.service;
 
+import com.foodlab.foodReservation.item.dto.request.CreateItemRequest;
 import com.foodlab.foodReservation.item.dto.request.UpdateItemRequest;
+import com.foodlab.foodReservation.item.dto.response.CreateItemResponse;
+import com.foodlab.foodReservation.item.dto.response.DeleteItemResponse;
 import com.foodlab.foodReservation.item.dto.response.UpdateItemResponse;
 import com.foodlab.foodReservation.item.entity.Item;
 import com.foodlab.foodReservation.item.repository.ItemRepository;
+import com.foodlab.foodReservation.store.entity.Store;
+import com.foodlab.foodReservation.store.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final StoreRepository storeRepository;
 
     @Transactional
     public UpdateItemResponse updateItem(Long itemId, UpdateItemRequest updateItemRequest) {
@@ -21,4 +28,27 @@ public class ItemService {
         item.updateItem(updateItemRequest.getName(), updateItemRequest.getPrice());
         return UpdateItemResponse.of(item);
     }
+
+    @Transactional
+    public CreateItemResponse createItem(CreateItemRequest createItemRequest) {
+        Store store = storeRepository.findById(createItemRequest.getStoreId()).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 음식점입니다."));
+        Item item = Item.builder()
+                .name(createItemRequest.getName())
+                .price(createItemRequest.getPrice())
+                .store(store)
+                .deleted(false)
+                .build();
+        Item savedItem = itemRepository.save(item);
+        return new CreateItemResponse(savedItem.getId());
+    }
+
+    @Transactional
+    public DeleteItemResponse deleteItem(Long itemId) {
+        Item item = itemRepository.findById(itemId).orElseThrow(
+                () -> new IllegalArgumentException("존재하지 않는 매뉴입니다."));
+        item.delete();
+        return new DeleteItemResponse(item.getId(), item.isDeleted());
+    }
+
 }
