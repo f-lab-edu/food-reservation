ce6789dc94b2a674090b3932046630a64b1e78b2
OldRabbit736 <sylvan0212@gmail.com>
2022-10-14 13:09:39 +0900

    Testcontainer 도입으로 더 이상 test 수행 시 docker compose를 수동으로 실행 시킬 필요가 없어졌으므로, - test-ci.sh, test-env.sh 삭제 - workflow에서 해당 sh 사용하는 부분 삭제

--- script/test-env.sh
+++
@@ -1,6 +0,0 @@
-# app 테스트를 수행하기 위한 환경을 세팅하는 스크립트 입니다.
-# app 서비스 제외한 나머지 서비스가 실행됩니다.
-docker compose -f docker-compose-test.yml --project-name food-reservation-test up
-
-# 스크립트를 수행하여 환경을 세팅한 후, 앱의 테스트를 실행해야 합니다.
-# 테스트 클래스에 ActiveProfile("test") 를 적어주어, test 프로파일을 이용하여 실행될 수 있도록 해야 합니다.
